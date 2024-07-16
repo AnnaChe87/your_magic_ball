23f09f074e6d201aa25d35dcad1d0a6894230b3d
Черных Анна <chernykhav@it2g.ru>
2024-07-16 10:19:43 +0400

    Respond to messages

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,4 +1,9 @@
+const axios = require("axios").default;
+
 exports.handler = async (event) => {
 	console.log('Received an update from Telegram!', event.body);
+	await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+  // TODO: Add form data
+});
 	return { statusCode: 200 }; 
 }
